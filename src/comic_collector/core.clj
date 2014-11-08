892cede776d823b6df21284e2b9a77ba79fd79f7
Ryan Johnson <brillianthamologist@gmail.com>
2014-11-08 09:47:57 -0600

    Moved the parsing functions to a separate namespace.  Modified the parser functions to not pass around the buy list as adding to the buy list is not the job of the parser.

--- src/comic_collector/core.clj
+++ src/comic_collector/core.clj
@@ -1,6 +1,7 @@
-(ns comic-collector.core)
+(ns comic-collector.core
+  (:require [comic-collector.parser :as parser]))
 
-(def start-publishers-line "PREMIER PUBLISHERS")
+
 
 (def titles-to-buy #{
                      "BLACK SCIENCE"
@@ -17,73 +18,23 @@
                      "MEN OF WRATH"
                      "THOR"})
 
-(def item-types #{"HC" "TP" "SC" "POSTER" "GN" "STATUE" "T/S" "HOODIE" "CASE" "DVD" "AF" "FIGURE"} )
 
-(declare handle-publisher)
 
 (defn create-shopping-list [] [])
 
-(defn add-to-shopping-list [item shopping-list]
+(defn- add-to-shopping-list [item shopping-list]
   (conj shopping-list item))
 
-(defn in-buy-list? [item]
+(defn- in-buy-list? [item]
   (contains? titles-to-buy (:name item)))
 
-(defn stringify [tokens]
-  (clojure.string/trim (clojure.string/join " " tokens)))
-
-(defn classify [token attrs]
-  (if (= \# (first token))
-    (assoc attrs :type "ISSUE" :number token)
-    (assoc attrs :type token)))
-
-(defn parse-name [tokens attributes]
-  (let [ [name other] (split-with
-                        (fn [t]
-                          (and
-                            (not (= \# (first t)))
-                            (not (contains? item-types t))))
-                        tokens)]
-    (classify (first other) (assoc attributes :name (stringify name) :notes (stringify (rest other))))))
-
-(defn parse-item-line [line publisher]
-  (let [tokens (clojure.string/split line #"\s")]
-    (parse-name (drop-last (drop 1 tokens)) {:id (first tokens) :publisher publisher :cost (last tokens)})))
-
-(defn check-list [item shopping-list]
+(defn- check-list [item shopping-list]
   (if (in-buy-list? item)
     (add-to-shopping-list item shopping-list)
     shopping-list))
-
-(defn handle-item [line publisher shopping-list]
-  (let [item (parse-item-line line publisher)]
-    (println item)
-    (check-list item shopping-list)))
-
-(defn handle-items [lines publisher shopping-list]
-    (loop [title-lines lines bl shopping-list]
-    (if (and title-lines (not (clojure.string/blank? (first title-lines))))
-        (recur (rest title-lines) (handle-item (first title-lines) publisher bl))
-      (handle-publisher title-lines bl))))
-
-(defn handle-publisher [lines shopping-list]
-  "Assumes that their are at least three lines.
-  The first is blank, the second is the name
-  of a publisher, and the third is another blank line."
-  (if (seq lines)
-    (let [
-          publisher (first (rest lines))
-          items (drop 3 lines)]
-        (handle-items items publisher shopping-list))
-    shopping-list))
-
-(defn handle-preamble [lines]
-  (if (= start-publishers-line (first lines))
-    (handle-publisher (rest lines) (create-shopping-list) )
-    (handle-preamble (rest lines))))
 
 (defn -main
   "Main function"
   [& args]
   (println (with-open [reader (clojure.java.io/reader "http://www.previewsworld.com/shipping/newreleases.txt")]
-    (handle-preamble (line-seq reader)))))
+    (parser/parse-file (line-seq reader)))))
---
+++ src/comic_collector/parser.clj
@@ -0,0 +1,62 @@
+(ns comic-collector.parser
+  (:require [clojure.string :as str]))
+
+(def start-publishers-line "PREMIER PUBLISHERS")
+
+(def item-types #{"HC" "TP" "SC" "POSTER" "GN" "STATUE" "T/S" "HOODIE" "CASE" "DVD" "AF" "FIGURE"} )
+
+
+(defn- stringify [tokens]
+  (str/trim (str/join " " tokens)))
+
+(defn- classify [token attrs]
+  (cond (= \# (first token)) (assoc attrs :type "ISSUE" :number token)
+        (not (nil? (:type attrs))) (assoc attrs :type token)
+        :else (assoc attrs :type "OTHER")))
+
+(defn- parse-name [tokens attributes]
+  (let [ [name other] (split-with
+                        (fn [t]
+                          (and
+                            (not (= \# (first t)))
+                            (not (contains? item-types t))))
+                        tokens)]
+    (classify (first other) (assoc attributes :name (stringify name) :notes (stringify (rest other))))))
+
+(defn- parse-item-line [line publisher]
+  (let [tokens (str/split line #"\s")]
+    (parse-name (drop-last (drop 1 tokens)) {:id (first tokens) :publisher publisher :cost (last tokens)})))
+
+
+
+(defn- handle-item [line publisher parsed-list]
+  (let [item (parse-item-line line publisher)]
+    (conj parsed-list item)))
+
+
+(declare handle-publisher)
+
+(defn- handle-items [lines publisher parsed-list]
+  (loop [title-lines lines pl parsed-list]
+    (if (and title-lines (not (str/blank? (first title-lines))))
+      (recur (rest title-lines) (handle-item (first title-lines) publisher pl))
+      (handle-publisher title-lines pl))))
+
+(defn- handle-publisher [lines parsed-list]
+  "Assumes that their are at least three lines.
+  The first is blank, the second is the name
+  of a publisher, and the third is another blank line."
+  (if (seq lines)
+    (let [
+          publisher (first (rest lines))
+          items (drop 3 lines)]
+      (handle-items items publisher parsed-list))
+    parsed-list))
+
+(defn- handle-preamble [lines]
+  (if (= start-publishers-line (first lines))
+    (handle-publisher (rest lines) [])
+    (handle-preamble (rest lines))))
+
+(defn parse-file [lines]
+  (handle-preamble lines))
